903f7e4c0259c495232937d7f5d386cd3da57ef7
Jarek Liesen <jarek.liesen@reuben.ox.ac.uk>
2026-01-21 23:05:25 +0000

    added struct to main namespace

--- src/envelope/__init__.py
+++ src/envelope/__init__.py
@@ -1,6 +1,7 @@
 from envelope.compat import create
 from envelope.environment import Environment, Info, InfoContainer
 from envelope.spaces import BatchedSpace, Continuous, Discrete, PyTreeSpace, Space
+from envelope.struct import field, static_field, FrozenPyTreeNode, Container
 from envelope.wrappers import (
     Wrapper,
     WrappedState,
@@ -24,6 +25,11 @@ __all__ = [
     "Continuous",
     "Discrete",
     "PyTreeSpace",
+    # Struct
+    "field",
+    "static_field",
+    "FrozenPyTreeNode",
+    "Container",
     # Wrappers
     "Wrapper",
     "WrappedState",
