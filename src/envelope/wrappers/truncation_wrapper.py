d08bc2ce4de74dfc6d3ba9ad390094456c97a807
Jarek Liesen <jarek.liesen@reuben.ox.ac.uk>
2026-02-04 17:02:12 +0000

    fix TruncationWrapper reset to not pass inner state
    When reset is called with an existing state, TruncationWrapper should
    reset the inner environment fresh (not pass the inner state), since
    the wrapper's role is just to track steps which get reset to 0.

--- src/envelope/wrappers/truncation_wrapper.py
+++ src/envelope/wrappers/truncation_wrapper.py
@@ -18,7 +18,7 @@ class TruncationWrapper(Wrapper):
         if state is None:
             inner_state, info = self.env.reset(key, **kwargs)
         else:
-            inner_state, info = self.env.reset(key, state.inner_state, **kwargs)
+            inner_state, info = self.env.reset(key, **kwargs)
         state = self.TruncationState(inner_state=inner_state, steps=0)
         return state, info.update(truncated=self.max_steps <= 0)
 
